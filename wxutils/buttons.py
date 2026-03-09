187f66696b0e6a92ad145db7049f456a822bc7b4
Christofanis Skordas <55315750+skordaschristofanis@users.noreply.github.com>
2026-03-09 09:12:08 -0500

    converts BitmapButton into a class and adds set and remove action methods

--- wxutils/buttons.py
+++ wxutils/buttons.py
@@ -19,13 +19,24 @@ class Button(wx.Button):
         self.Unbind(wx.EVT_BUTTON, handler=action)
 
 
-def BitmapButton(parent, bmp, action=None, tooltip=None, size=(20, 20), **kws):
-    b = wx.BitmapButton(parent, id=-1, bitmap=bmp, size=size, **kws)
-    if action is not None:
-        parent.Bind(wx.EVT_BUTTON, action, b)
-    if tooltip is not None:
-        b.SetToolTip(tooltip)
-    return b
+class BitmapButton(wx.BitmapButton):
+    def __init__(
+        self, parent: wx.Window, bmp: wx.Bitmap, action: Optional[Callable] = None, tooltip: Optional[str] = None, size: tuple[int, int] = (20, 20), **kws
+    ) -> None:
+        bitmap_arg = wx.BitmapBundle.FromBitmap(bmp) if isinstance(bmp, wx.Bitmap) else bmp
+        super(BitmapButton, self).__init__(parent=parent, id=-1, bitmap=bitmap_arg, size=wx.Size(size[0], size[1]), **kws)
+
+        if action is not None:
+            self.SetAction(action)
+        if tooltip is not None:
+            self.SetToolTip(tooltip)
+
+    def SetAction(self, action: Callable) -> None:
+        self.Bind(wx.EVT_BUTTON, handler=action)
+
+    def RemoveAction(self, action: Callable) -> None:
+        self.Unbind(wx.EVT_BUTTON, handler=action)
+
 
 def ToggleButton(parent, label, action=None, tooltip=None,
                  size=(25, 25), **kws):
